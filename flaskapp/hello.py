a4b1c1d2750701119de619bcc054b46dda9d1ab6
pankaja-shree <pankajashree@gmail.com>
2017-09-27 10:50:45 +0530

    define authenticate route

--- flaskapp/hello.py
+++ flaskapp/hello.py
@@ -1,3 +1,6 @@
+from flask import Flask, request
+from flaskext.mysql import MySQL
+
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -5,6 +8,18 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root@123'
 app.config['MYSQL_DATABASE_DB'] = 'employee'
 app.config['MYSQL_DATABASE_HOST'] = '192.168.1.80'
 mysql.init_app(app)
+
+@app.route("/Authenticate")
+def Authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+     return "Username or Password is wrong"
+    else:
+     return "Logged in successfully"
 
 @app.route("/")
 def hello():
