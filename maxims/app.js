32f137c85775ebd7216ae8756d698c14db0398a3
pankaja-shree <pankajashree@gmail.com>
2017-09-28 11:58:03 +0530

    create GET request and route

--- maxims/app.js
+++ maxims/app.js
@@ -1,10 +1,39 @@
-const express = require('express'),
+var express = require('express'),
+bodyParser = require("body-parser")//,
+    //db = require('./db.js')
 
-const app = express()
+var mysql = require('mysql');
+var http = require('http').Server(app);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: "192.168.1.80",
+  user: "root",
+  password: "root@123",
+  database: "sanskrit"
+});
+
+//print all maxims
+app.get('/', (req, res) => {
+  var data = {
+    "Data":""
+};
+  con.query('SELECT * FROM maxims', (err, rows, fields) => {
+    if (err) console.log(err)
+    if(rows.length != 0){
+      data["Data"] = rows;
+      res.json(data);
+  }else{
+      data["Data"] = 'No data Found..';
+      res.json(data);
+  }
+   })
 })
+
+//post a maxim
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
