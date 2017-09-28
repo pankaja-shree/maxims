9c50df1b7334fa96391d806487ed8b30d5e9a1e4
pankaja-shree <pankajashree@gmail.com>
2017-09-28 11:58:15 +0530

    create db helper function

--- maxims/db.js
+++ maxims/db.js
@@ -1,12 +1,18 @@
-mysql = require('mysql');
+var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "192.168.1.80",
     user: "root",
-    password: "root@123"
+    password: "root@123",
+    database: "sanskrit"
   });
 
 con.connect(function(err) {
-    if (err) throw err;
+    if (err) console.log(err);
     console.log("Connected to db!");
   });
+
+exports.getmaxims = con.query('SELECT * FROM maxims', (err, rows, fields) => {
+  if (err) return err
+  return rows[0].solution
+ })
